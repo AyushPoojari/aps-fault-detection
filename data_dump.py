f5301b760376251e24a19b0bc656e05c8900734f
Ayush Poojari <ayushp.b11@gmail.com>
2022-12-21 12:14:53 +0530

    Added Airflow

--- data_dump.py
+++ data_dump.py
@@ -5,9 +5,9 @@ Loading data to MongoDB
 import pymongo
 import pandas as pd
 import json
+from dotenv import load_dotenv
+from sensor.config import mongo_client
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
 DATA_FILE_PATH = "/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME = "aps"
@@ -21,4 +21,4 @@ if __name__ == "__main__":
     df.reset_index(drop=True,inplace=True)
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
